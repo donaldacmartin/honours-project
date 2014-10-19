c8b7b7f9cbc4b8d2fac78c4ff563c6e9e949026c
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-19 12:00:01 +0100

    Back to basics

--- main.py
+++ main.py
@@ -6,49 +6,27 @@
 
 from os import listdir
 from subprocess import check_output, STDOUT
+from models import AutonomousSystem
+
+"""
+Will be useful later
 
 PATH = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/"
 CMD  = "bgpdump -m <filename>"
+"""
 
-as_paths = {}
-
-def is_bgpdump_installed():
+def read_file(filename):
     try:
-        output = check_output("bgpdump", shell=True, stderr=STDOUT)
-    except Exception, e:
-        output = str(e.output)
+        file = open(filename, "r")
+        contents = file.read()
+        file.close()
+        return contents
+    except:
+        print("Error opening file " + filename)
+        exit()
+        
+def split_file_into_lines(file_contents):
+    return file_contents.split("\n")
     
-    return "bgpdump version" in output
-    
-def get_all_folders():
-    available_months = listdir(PATH)
-    dump_path = PATH + available_months[0] + "/RIBS/"
-    available_dumps  = listdir(dump_path)
-    output = dump_path + available_dumps[0]
-    return output
-
-def to_ascii(cmd):
-    try:
-        output = check_output(cmd, shell=True, stderr=STDOUT)
-    except Exception, e:
-        output = str(e.output)
-    
-    lines = output.split("\n")
-    
-    for line in lines:
-        try:
-            path = line.split("|")[6]
-            hops = path.split(" ")
-            
-            if hops[-1] in as_paths:
-                as_paths[hops[-1]].append(hops)
-            else:
-                as_paths[hops[-1]] = [hops]
-        except:
-            print("Unable to get line")
-
-to_ascii("bgpdump -m " + get_all_folders())
-
-while True:
-    k = input()
-    print(as_paths(k))
+data = split_file_into_lines(read_file("/home/1101795m/output.out"))
+print("Data read in successfully")
