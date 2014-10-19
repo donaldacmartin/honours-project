694937e27ff31b6b63d7eef115db66362bd2346e
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-19 12:08:15 +0100

    Extracting paths from data

--- main.py
+++ main.py
@@ -25,6 +25,23 @@ def read_file(filename):
         
 def split_file_into_lines(file_contents):
     return file_contents.split("\n")
+
+def get_path_from_line(line):
+    try:
+        return line.split("|")[6]
+    except:
+        return None
     
 data = split_file_into_lines(read_file("/home/1101795m/output.out"))
 print("Data read in successfully")
+
+paths = []
+
+for line in data:
+    path = get_path_from_line(line)
+    
+    if path is not None:
+        paths.append(path)
+    
+print("Paths extracted successfully")
+print(str(len(paths)) + " paths found")
