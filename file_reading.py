933cb430e6b411c16cdbcc4b2a3500a7225bf5f7
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-20 12:34:50 +0100

    added file path

--- file_reading.py
+++ file_reading.py
@@ -9,6 +9,7 @@ from tempfile import NamedTemporaryFile
 
 PATH = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/"
 CMD  = "bgpdump -m <filename>"
+file_path = "2001.10/RIBS/rib.20011026.1648.bz2"
 
 output = NamedTemporaryFile()
 check_call([CMD, PATH + file_path], stdout=output, stderr=STDOUT)
