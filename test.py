46f376ddfa9a0cd6da8367972e2d21a5514187aa
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-20 15:45:45 +0100

    Multithreading attempt

--- test.py
+++ test.py
@@ -6,6 +6,7 @@
 
 from time import time
 from file import FileReader
+from threading import Thread
 
 def run_speed_test(filename):
     start = time()
@@ -13,6 +14,16 @@ def run_speed_test(filename):
     end = time()
     print("Time taken for " + filename + " was " + str(end - start))
 
+def thread_test(filenames):
+    for i in range(len(filenames)):
+        t = Thread(target=run_speed_test, args=(filenames[i]))
+        t.start()
+
+start = "2001.10/RIBS/rib.20011026."
+tests = [start+"1648.bz2",start+"1848.bz2",start+"2048.bz2",start+"2248.bz2"]
+thread_test(tests)
+
+"""        
 run_speed_test("2001.10/RIBS/rib.20011026.1648.bz2")
 run_speed_test("2001.10/RIBS/rib.20011026.1848.bz2")
 run_speed_test("2001.10/RIBS/rib.20011026.2048.bz2")
@@ -22,3 +33,4 @@ run_speed_test("2014.06/RIBS/rib.20140601.0000.bz2")
 run_speed_test("2014.06/RIBS/rib.20140601.0200.bz2")
 run_speed_test("2014.06/RIBS/rib.20140601.0400.bz2")
 run_speed_test("2014.06/RIBS/rib.20140601.0600.bz2")
+"""
