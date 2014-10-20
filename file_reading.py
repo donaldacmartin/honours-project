c06d281625018dd68f9875c8f5087f028d5e5e11
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-20 13:14:30 +0100

    Using the Popen approach

--- file_reading.py
+++ file_reading.py
@@ -4,6 +4,7 @@
 # Map of the Internet
 # Donald Martin (1101795)
 
+"""
 import os
 from subprocess import check_call, STDOUT
 from tempfile import NamedTemporaryFile
@@ -11,14 +12,19 @@ from tempfile import NamedTemporaryFile
 PATH = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/"
 CMD  = "bgpdump -m " + PATH
 file_path = "2001.10/RIBS/rib.20011026.1648.bz2"
-"""
+
 print("File : " + PATH + file_path)
 output = NamedTemporaryFile()
 check_call([file_path], stdout=output, stderr=STDOUT)
 output.seek(0)
 print(output.read())
 """
-with NamedTemporaryFile() as f:
-    check_call([CMD, os.path.abspath(PATH + file_path)], stdout=f, stderr=STDOUT)
-    f.seek(0)
-    print(f.read())
+
+import shlex, subprocess
+
+commands = "bgpdump -m /nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/2001.10/RIBS/rib.20011026.1648.bz2"
+args = shlex.split(commands)
+p = subprocess.Popen(args, stdout=subprocess.PIPE)
+out, err = p.communicate()
+
+print(out)
