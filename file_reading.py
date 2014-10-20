a0d22e83ae8fbfc05cfdd50b5c3c8948da5e030a
Donald AC Martin <donaldacmartin@gmail.com>
2014-10-20 14:37:13 +0100

    OOification

--- file_reading.py
+++ file_reading.py
@@ -17,16 +17,52 @@ class FileReader():
         self.args = split(COMMAND + PATH + filename)
         self.lock = Lock()
         self.read_binary_into_ascii()
+        self.nodes = {}
         
     def read_binary_into_ascii(self):
         with self.lock:
             p = Popen(self.args, stdout=self.temp_file)
             p.communicate()
-            self.temp_file.seek(0)
+            self.split_into_paths()
             
-    def read_line(self):
-        with self.lock:
-            return self.temp_file.readline()
+    def strip_invalid_entries(hops):
+        if "{" in hops[-1]:
+            del hops[-1]
+        
+        return hops
+    
+    def add_connection(self, node1, node2):
+        if node1 not in self.nodes:
+            self.nodes[node1] = []
+        
+        if node2 not in self.nodes[node1]:
+            self.nodes[node1].append(node2)
+    
+    def append_to_nodes(self, hops):
+        counter = 0
+        
+        while counter < len(hops):
+            this_node = int(hops[counter])
+            
+            if counter + 1 < len(hops):
+                self.add_connection(this_node, int(hops[counter + 1]))
+                
+            if counter - 1 > 0:
+                self.add_connection(this_node, int(hops[counter - 1]))
+                
+            counter += 1
+    
+    def split_into_paths(self):
+        self.temp_file.seek(0)
+        data = self.temp_file.readline()
+        
+        while data != "" and data is not None:
+            path = data.split("|")[6]
+            hops = strip_invalid_entries(path.split(" "))
+            self.append_to_nodes(hops)
+            
+    def get_entry(self, num):
+        return self.nodes[num]
             
 f = FileReader("2001.10/RIBS/rib.20011026.1648.bz2")
-print(f.read_line())
+print(f.get_entry(1))
