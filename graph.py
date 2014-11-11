844a6915a7c880c3bbc729ebf87af04cd9585aba
Donald Martin <1101795m@batudaka.dcs.gla.ac.uk>
2014-11-11 13:26:30 +0000

    Update

--- graph.py
+++ graph.py
@@ -14,21 +14,22 @@ import matplotlib.pyplot as plt
 from networkx import Graph, ego_graph, graphviz_layout, draw
 
 class NetworkGraph():
-    def __init__(self, as_links):
+    def __init__(self, as_links, size, file_name):
         self.graph = Graph()
+	self.size = size
+	self.file = file_name
         self.__add_edges(as_links)
         self.__draw()
     
-    def __add_edges(self, as_links):
-        counter = 200
-        
+    def __add_edges(self, as_links):        
         for link in as_links:
             self.graph.add_edge(link[0], link[1])
-            counter -= 1
+            self.size -= 1
             
-            if counter < 0:
+            if self.size < 0:
                 break
             
     def __draw(self):
-        draw(self.graph, node_size=30)
-        plt.savefig('graph_viz_graph.png')
+	plt.figure(figsize=(8,8))
+        draw(self.graph, node_size=10)
+        plt.savefig(self.file)
--- main.py
+++ main.py
@@ -4,9 +4,22 @@
 # Map of the Internet
 # Donald Martin (1101795)
 
+import time
 from file import BGPDumpExecutor
 from graph import NetworkGraph
 
+def write_output(counter, timer):
+	with open("test.out", "a") as file:
+		file.write(str(counter) + str(timer) + "\n")
+
 file     = "bgpdata/2001.10/RIBS/rib.20011031.2234.bz2"
-as_links = BGPDumpExecutor(file).get_connections()
-NetworkGraph(as_links)
+file2	 = "bgpdata/2014.06/RIBS/rib.20140630.2200.bz2"
+as_links = BGPDumpExecutor(file2).get_connections()
+
+test_cases = [3000000000]
+
+for test in test_cases:
+	start = time.time()
+	NetworkGraph(as_links, test, str(test) + ".png")
+	end = time.time()
+	write_output(test, end-start)
