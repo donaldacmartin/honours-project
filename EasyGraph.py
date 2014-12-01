77ce94f2b42ac330cfe108b79b7e651ab34ca39d
Donald AC Martin <donald@martin.scot>
2014-12-01 12:58:55 +0000

    lol

--- EasyGraph.py
+++ EasyGraph.py
@@ -61,7 +61,7 @@ class StaggeredRingGraph(RingGraph):
         angle = float(0)
         
         for asys in self.links:
-            radius = max_radius * (len(self.links[asys]) / max_radius)
+            radius = max_radius * (len(self.links[asys]) / max_cxns)
             print(radius)
             
             x = centre[0] - (radius * sin(angle))
