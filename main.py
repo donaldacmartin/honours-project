8c68f041a3941d90c84ed77992bcc06000b6d042
Donald AC Martin <donald@martin.scot>
2014-12-01 11:53:19 +0000

    lol

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ pickle.dump(connections, open("sampleset", "wb"))
 """
 
 connections = pickle.load(open("sampleset", "rb"))
-gp = RingGraph(20240, 146800)
+gp = RingGraph(20000, 20000)
 
 for cxn in connections:
     gp.add_link(cxn[0], cxn[1])
