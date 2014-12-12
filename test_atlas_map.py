225414d9a4a8c9f032d263deb9c96da595943f1d
Donald AC Martin <donald@martin.scot>
2014-12-12 15:43:29 +0000

    Hotfix

--- test_atlas_map.py
+++ test_atlas_map.py
@@ -16,8 +16,8 @@ import logging
 class AtlasMapTest(unittest.TestCase):
     def setUp(self):
         self.expected_image  = Image.open("test_cases/simple_atlas_map.png")
-        self.simple_bgp_file = "/nas05/users/csp/routing-data/" +
-                               "archive.routeviews.org/bgpdata/2001.10/RIBS/" +
+        self.simple_bgp_file = "/nas05/users/csp/routing-data/"
+                               "archive.routeviews.org/bgpdata/2001.10/RIBS/"
                                "rib.20011027.0849.bz2"
         
         self.bgp_dump = BGPDumpExecutor(self.simple_bgp_file)
