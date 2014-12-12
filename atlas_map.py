d2b21c0810fa1dd9b0249e1b020372dfb69410de
Donald AC Martin <donald@martin.scot>
2014-12-12 14:13:29 +0000

    error logging

--- atlas_map.py
+++ atlas_map.py
@@ -28,13 +28,17 @@ class AtlasMap(object):
         self.image = new("RGB", (width, height), "white")
         
     def add_auto_sys_ip(self, as_num, ip_address):
-        lat,lon       = self.geoip.get_latlon_for_ip(ip_address)
-        width, height = self.image.size
+        try:
+            lat,lon       = self.geoip.get_latlon_for_ip(ip_address)
+            width, height = self.image.size
+            
+            x = map_lon_to_x_coord(lon, width)
+            y = map_lat_to_y_coord(lat, height)
+            
+            self.coords_for_asys[as_num] = (x,y)
+        except NameError as e:
+            logging.warning("No LatLon for" + str(as_num) + e)
         
-        x = map_lon_to_x_coord(lon, width)
-        y = map_lat_to_y_coord(lat, height)
-        
-        self.coords_for_asys[as_num] = (x,y)
         
     def add_link(self, start, end):
         start = int(start)
