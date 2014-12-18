b22753b808ea2c779a8dd1e425c25214c23353a8
Donald AC Martin <donald@martin.scot>
2014-12-18 17:59:32 +0000

    A lot of processing and semaphores

--- high_level/monthly.py
+++ high_level/monthly.py
@@ -8,63 +8,24 @@ from utilities.threads import run_bgp_dump, generate_chrono_map
 from utilities.file_search import get_bgp_binaries_in
 from utilities.images2gif import writeGif
 from graphs.chrono_atlas_map import ChronologicalAtlasMap
-from multiprocessing import Process
+from multiprocessing import Process, Semaphore
+from utilities.bgp import BGPDumpExecutor
 
 def generate_monthly_diff():
-    bgp_files          = __get_list_of_files()
-    threads, bgp_dumps = run_bgp_dump(bgp_files)
-    asys_coords        = {}
-    
-    for i in range(1, len(threads)):
-        args = (bgp_files[i-1], bgp_files[i], bgp_dumps, asys_coords)
-        proc = Process(target=thread, args=args)
-        proc.start()
-    
-def thread(file1, file2, bgp_dumps, asys_coords):
-    print("Drawing")
-    prev_cxns = bgp_dumps[file1].as_connections
-    curr_cxns = bgp_dumps[file2].as_connections
-    
-    prev_ips  = bgp_dumps[file1].as_to_ip_address
-    curr_ips  = bgp_dumps[file2].as_to_ip_address
-    
-    chrono = ChronologicalAtlasMap(20000, 10000, prev_cxns, curr_cxns, prev_ips, curr_ips, asys_coords)
-    chrono.save(str(i) + ".png")
-    print("Finished")
-    
-def generate_monthly_diff_single_thread():
-    bgp_files   = __get_list_of_files()
+    files       = __get_list_of_files()
+    semaphores  = [Semaphore(2) for _ in range(len(files))]
     bgp_dumps   = {}
     asys_coords = {}
-    output_imgs = []
     
-    for i in range(1, len(bgp_files)):
-        print("Starting " + str(i) + " of " + str(len(bgp_files)))
-        prev_path = bgp_files[i-1]
-        curr_path = bgp_files[i]
+    for i in range(len(files)):
+        args = (files[i], bgp_dumps, semaphores[i],)
+        proc = Process(target=__bgp_process, args=args)
+        proc.start()
         
-        files_to_parse = []
-        
-        if prev_path not in bgp_dumps:
-            files_to_parse.append(prev_path)
-            
-        if curr_path not in bgp_dumps:
-            files_to_parse.append(curr_path)
-            
-        new_dumps = run_bgp_dump(files_to_parse)
-        bgp_dumps = dict(list(bgp_dumps.items()) + list(new_dumps.items()))
-        
-        prev_cxns = bgp_dumps[prev_path].as_connections
-        curr_cxns = bgp_dumps[curr_path].as_connections
-        
-        prev_ips  = bgp_dumps[prev_path].as_to_ip_address
-        curr_ips  = bgp_dumps[curr_path].as_to_ip_address
-        
-        chrono = ChronologicalAtlasMap(1920,1080, prev_cxns, curr_cxns, prev_ips, curr_ips, asys_coords)
-        chrono.save(str(i) + ".png")
-        output_imgs.append(chrono.image)
-        
-    writeGif("big.gif", output_imgs, duration=0.5)
+    for i in range(1, len(files)):
+        args = (files[i-1], files[i], semaphores[i-1], semaphores[i], asys_coords, i,)
+        proc = Process(target=__chrono_map_process, args=args)
+        proc.start()
     
 def __get_list_of_files():
     base_dir  = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/"
@@ -89,3 +50,37 @@ def __filter_a_file(files, month, year):
             return bgp_file
             
     return None
+
+def __bgp_process(filename, bgp_dumps, semaphore, counter):
+    semaphore.acquire()
+    semaphore.acquire()
+    
+    print("Starting to get BGP for " + str(counter))
+    
+    bgp = BGPDumpExecutor(filename)
+    bgp_dumps[filename] = bgp
+    
+    print("Finished BGP for " + str(counter))
+    
+    semaphore.release()
+    semaphore.release()
+    
+def __chrono_map_process(prev_filename, curr_filename, prev_semaphore, curr_semaphore, bgp_dumps, asys_coords, counter):
+    prev_semaphore.acquire()
+    curr_semaphore.acquire()
+    
+    print("Starting to graph for " + str(counter))
+    
+    prev_cxns = bgp_dumps[prev_filename].as_connections
+    curr_cxns = bgp_dumps[curr_filename].as_connections
+    
+    prev_addr = bgp_dumps[prev_filename].as_to_ip_address
+    curr_addr = bgp_dumps[curr_filename].as_to_ip_address
+    
+    chrono = ChronologicalAtlasMap(20000,10000, prev_cxns, curr_cxns, prev_addr, curr_addr, asys_coords)
+    chrono.save(str(counter) + ".png")
+    
+    print("Finished graph for " + str(counter))
+    
+    curr_semaphore.release()
+    prev_semaphore.release()
