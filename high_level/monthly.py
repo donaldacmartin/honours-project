f385588e83f1aaa7f4a4c5fa835539e640a901d4
Donald AC Martin <donald@martin.scot>
2014-12-18 20:29:44 +0000

    lots of procs

--- high_level/monthly.py
+++ high_level/monthly.py
@@ -7,48 +7,39 @@
 from utilities.file_search import get_bgp_binaries_in
 from utilities.images2gif import writeGif
 from graphs.chrono_atlas_map import ChronologicalAtlasMap
-from multiprocessing import Process, Semaphore, Manager, Pool
+from multiprocessing import Process, Semaphore, Manager
 from utilities.bgp import BGPDumpExecutor
 
 def generate_monthly_diff():
     files       = __get_list_of_files()
     semaphores  = [Semaphore(2) for _ in range(len(files))]
-    #processes   = []
+    processes   = []
     manager     = Manager()
     bgp_dumps   = manager.dict()
     asys_coords = manager.dict()
     
-    pool = Pool()
-    
     for i in range(len(files)):
         args = (files[i], bgp_dumps, semaphores[i], i,)
-        pool.apply_async(__bgp_process, args=args)
-        #proc = Process(target=__bgp_process, args=args)
-        #proc.start()
-        #processes.append(proc)
+        proc = Process(target=__bgp_process, args=args)
+        proc.start()
+        processes.append(proc)
     
     for i in range(1, len(files)):
         args = (files[i-1], files[i], semaphores[i-1], semaphores[i], bgp_dumps, asys_coords, i,)
         pool.apply_async(__chrono_map_process, args=args)
-        #proc = Process(target=__chrono_map_process, args=args)
-        #proc.start()
-        #processes.append(proc)
+        proc = Process(target=__chrono_map_process, args=args)
+        proc.start()
+        processes.append(proc)
         
-    #for proc in processes:
-        #proc.join()
-    
-    pool.close()
-    pool.join()
-    
-def __log_res(res):
-    pass
+    for proc in processes:
+        proc.join()
     
 def __get_list_of_files():
     base_dir  = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/"
     all_files = get_bgp_binaries_in(base_dir)
     months    = []
     
-    for year in range(2001, 2002):
+    for year in range(2001, 2014):
         for month in range(1, 13):
             filename = __filter_a_file(all_files, month, year)
             
