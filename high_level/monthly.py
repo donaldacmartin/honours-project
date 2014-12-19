11b71205f5f7160dcb0bddb12c480451aee127e7
Donald AC Martin <donald@martin.scot>
2014-12-19 11:51:28 +0000

    assignment

--- high_level/monthly.py
+++ high_level/monthly.py
@@ -21,7 +21,7 @@ def generate_monthly_diff():
     
     for i in range(len(files)):
         args = (files[i], bgp_dumps, semaphores[i], i)
-        pool.apply_async(__bgp_process, args)
+        result = pool.apply_async(__bgp_process, args)
         #proc = Process(target=__bgp_process, args=args)
         #proc.start()
         #processes.append(proc)
