8c22a911eeb5e1e540982a55eeeabb2e823eee83
Donald AC Martin <donald@martin.scot>
2014-12-23 13:18:14 +0000

    .

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ bgp1 = BGPDumpExecutor(base + "2001.10/RIBS/rib.20011026.1648.bz2")
 #bgp2 = BGPDumpExecutor(base + "2002.10/RIBS/rib.20021026.1517.bz2")
 
 a = AtlasMap(1920, 1080, bgp1)
-a.save("atlas.png")
+a.save("atlas-no-limits.png")
+
+b = AtlasMap(1920, 1080, ((53.76, -137.37), (23.43, -53.17)), bgp1)
+b.save("atlas-limits.png")
 
 """
 c = ChronoAtlasMap(1920, 1080, bgp1, bgp2)
 c.save("chrono.png")
-"""
 
 r = RingGraph(1080, bgp1)
 r.save("ring.png")
+"""
