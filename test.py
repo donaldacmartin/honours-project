269e9e5cb4d7c0661829e3b4c9457dbbd868389b
Donald AC Martin <donald@martin.scot>
2014-12-23 16:44:44 +0000

    .

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from utilities.shapefile import Reader
 from Image import new
 from ImageDraw import Draw
+from graphs.atlas.atlas_map import map_lat_to_y_coord, map_lon_to_x_coord
 
 img = new("RGB", (400, 400), "white")
 draw = Draw(img)
@@ -14,7 +15,11 @@ for shape in reader.shapeRecords():
     for point in points:
         lon = point[0]
         lat = point[1]
-        outline.append((lon, lat))
+        
+        x = map_lon_to_x_coord(lon, 400)
+        y = map_lat_to_y_coord(lat, 400)
+        
+        outline.append((x,y))
         
     draw.polygon(outline, outline="black")
     
