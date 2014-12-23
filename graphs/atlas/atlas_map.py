92b9f33faf0bad04bf3460d22019b3521cba9fac
Donald AC Martin <donald@martin.scot>
2014-12-23 15:27:34 +0000

    .

--- graphs/atlas/atlas_map.py
+++ graphs/atlas/atlas_map.py
@@ -75,8 +75,8 @@ class AtlasMap(Graph):
         y_scale = img_height / abs(dy)
 
         for (asys, (x,y)) in self.asys_coords.items():
-            new_x = (x * x_scale) #- x_anchor
-            new_y = (y * y_scale) #- y_anchor
+            new_x = (x * x_scale) - x_anchor
+            new_y = (y * y_scale) - y_anchor
             self.asys_coords[asys] = (new_x, new_y)
             
     def _draw_line(self, start, end, colour):
