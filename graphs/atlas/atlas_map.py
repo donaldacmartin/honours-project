1204e2311c93c0f7aa9be8cfa66efb11a2597636
Donald AC Martin <donald@martin.scot>
2014-12-23 17:05:59 +0000

    .

--- graphs/atlas/atlas_map.py
+++ graphs/atlas/atlas_map.py
@@ -6,7 +6,8 @@
 
 from utilities.geoip import GeoIPLookup
 from graphs.graph import Graph, DARK_RED
-from utilities.border_mapper import draw_borders
+from utilities.shapefile import Reader
+from ImageDraw import Draw
 
 """
 AtlasMap
@@ -135,3 +136,29 @@ def coord_missing(start, end, coords):
         
 def start_closer_to_RHS(start_x, img_width):
     return (img_width - start_x) < start_x
+    
+def draw_borders(img, (limit1, limit2)=None):
+    reader = Reader("utilities/data/country_outlines/countries")
+    draw = Draw(img)
+    
+    x1 = map_lon_to_x_coord(limit1[1], img.size[0])
+    x2 = map_lon_to_x_coord(limit2[1], img.size[0])
+
+    y1 = map_lat_to_y_coord(limit1[0], img.size[1])
+    y2 = map_lat_to_y_coord(limit2[0], img.size[1])
+
+    x_anchor = min(x1, x2)
+    y_anchor = min(y1, y2)
+
+    x_scale = img_width / abs(x2 - x1)
+    y_scale = img_height / abs(y2 - y1)
+
+    for record in reader.shapeRecords():
+        points  = record.shape.points
+        outline = []
+        
+        for (lon, lat) in points:
+            x = (map_lon_to_x_coord(lon, img.size[0]) - x_anchor) * x_scale
+            y = (map_lat_to_y_coord(lat, img.size[1]) - y_anchor) * y_scale
+            
+        draw.polygon(outline, outline="black")
