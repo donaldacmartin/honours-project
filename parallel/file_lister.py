bd2ace89a585a610e36c71aa1dcc502f05c32386
Donald AC Martin <donald@martin.scot>
2015-01-08 15:46:51 +0000

    .

--- parallel/file_lister.py
+++ parallel/file_lister.py
@@ -9,20 +9,34 @@ from os import mkdir
 
 def list_files():
     files = FileBrowser("/nas05/users/csp/routing-data/archive.routeviews.org")
-    dumps = []
+
+    all_files    = []
+    yearly_files = []
 
     for year in range(1997, 2014):
-        dumps += files.get_year_end_files(year)
+        this_year = files.get_year_end_files(year)
+        all_files += this_year
+        yearly_files.append(this_year)
 
-    output(dumps)
+    output_all(dumps)
+    output_years(yearly_files)
 
-def output(files):
-    mkdir("temp")
+def output_all(files):
     output_file = open("temp/dumps", "w+")
 
     for dump in files:
         output_file.write(dump + "\n")
 
+    output_file.close()
+
+def output_years(yearly_files):
+    output_file = open("temp/years", "w+")
+
+    for year in yearly_files:
+        for f in year:
+            output_file.write(dump + "|")
+
+        output_file.write("\n")
     output_file.close()
 
 if __name__ == "__main__":
--- parallel/parser.py
+++ parallel/parser.py
@@ -4,18 +4,19 @@
 # Map of the Internet
 # Donald Martin (1101795)
 
-from utilities.bgp import BGPDumpExecutor
+from utilities.parser.bgp import BGPParser
+from utilities.parser.cisco import CiscoParser
 from pickle import dump
 from sys import argv
 
-def parse_bgp(filename):
-    bgp = BGPDumpExecutor(filename)
-
+def dump_data(filename, parser):
     output_filename = "temp/" + filename.replace("/", "_")
     output_file     = open(output_filename, "wb")
 
-    dump(bgp, output_file)
+    dump(parser, output_file)
     output_file.close()
 
 if __name__ == "__main__":
-    parse_bgp(argv[1])
+    filename = argv[1]
+    dump = BGPParser(filename) if "rib" in filename else CiscoParser(filename)
+    dump_data(filename, dump)
