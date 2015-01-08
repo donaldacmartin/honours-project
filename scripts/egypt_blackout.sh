090fa6d83a32dc83a487ef94786b667824ea4f32
Donald AC Martin <donald@martin.scot>
2015-01-08 19:00:47 +0000

    .

--- scripts/egypt_blackout.sh
+++ scripts/egypt_blackout.sh
@@ -17,3 +17,7 @@ parallel --no-notice -a temp/parsing_index "python $WORKING_DIR/parse_file.py"
 echo "Merging parsed outputs"
 mkdir temp/merged
 parallel --no-notice --xapply -a temp/merging_index "python $WORKING_DIR/merge_dumps.py"
+
+echo "Generating graph frames"
+mkdir graphs
+parallel --no-notice --xapply -a temp/diff_index_1 -a temp/diff_index_2 "python $WORKING_DIR/draw_graph.py"
