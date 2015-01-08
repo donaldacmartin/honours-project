6cd730dc7c29cb954519e8b70c086d26c8a727b1
Donald AC Martin <donald@martin.scot>
2015-01-08 13:38:10 +0000

    .

--- utilities/file_search.py
+++ utilities/file_search.py
@@ -20,7 +20,10 @@ class FileBrowser(object):
         files = self._load_all_files(directory)
         files = self._filter_files(files)
 
-        self.database = self._organise_into_dates(files)
+        self.oix_dumps = {}
+        self.rib_dumps = {}
+
+        self._organise_into_dates(files)
 
     def _load_all_files(self, directory):
         files  = []
@@ -34,48 +37,31 @@ class FileBrowser(object):
         return files
 
     def _filter_files(self, files):
-        return [file for file in files if f.endswith(".bz2")]
+        return [file for file in files if file.endswith(".bz2")]
 
     def _organise_into_dates(self, files):
-        chrono_listing = {}
-
         for filename in files:
-            yy, mm, dd, hh = self._organise_file(filename)
-
-            if yy is None:
-                continue
-
-            if (yy, mm, dd, hh) not in chrono_listing:
-                chrono_listing[(yy, mm, dd, hh)] = set()
-
-            chrono_listing[(yy, mm, dd, hh)].add(filename)
-
-        return chrono_listing
-
-    def _organise_file(self, filename):
-        if "oix" in filename:
-            return self._map_oix_filename(filename)
-        elif "RIBS" in filename:
-            return self._map_routeviews_filename(filename)
-
-        return (None, None, None, None)
+            if "oix" in filename:
+                self._map_oix_filename(filename)
+            elif "RIBS" in filename:
+                self._map_rib_filename(filename)
 
     def _map_oix_filename(self, filename):
         tokens = filename.split("-")
 
-        year  = int(tokens[-4])
-        month = int(tokens[-3])
-        date  = int(tokens[-2])
-        hour  = int(tokens[-1].split(".")[0][0:2])
+        yy = int(tokens[-4])
+        mm = int(tokens[-3])
+        dd = int(tokens[-2])
+        hh = int(tokens[-1].split(".")[0][0:2])
 
-        return year, month, date, hour
+        self.oix_dumps[(yy, mm, dd, hh)] = filename
 
-    def _map_routeviews_filename(self, filename):
+    def _map_rib_filename(self, filename):
         tokens = filename.split(".")
 
-        year  = int(tokens[-3][0:4])
-        month = int(tokens[-3][4:6])
-        date  = int(tokens[-3][6:8])
-        hour  = int(tokens[-2][0:2])
+        yy = int(tokens[-3][0:4])
+        mm = int(tokens[-3][4:6])
+        dd = int(tokens[-3][6:8])
+        hh = int(tokens[-2][0:2])
 
-        return year, month, date, hour
+        self.rib_dumps[(yy, mm, dd, hh)] = filename
