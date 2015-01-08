ae0fa5a72026236b1ad99838be3932300dd3acf7
Donald AC Martin <donald@martin.scot>
2015-01-08 13:31:59 +0000

    Added file browser

--- utilities/file_search.py
+++ utilities/file_search.py
@@ -15,24 +15,67 @@ are identified by the filename format rib.YYMMDD.HHMM.bz2. Paths are returned
 as a list of strings.
 """
 
-def get_bgp_binaries_in(directory):
-    regex      = compile("rib\.\d+\.\d+\.bz2")
-    bgp_files  = []
-    dir_walker = walk(directory)
+class FileBrowser(object):
+    def __init__(self, directory):
+        files = self._load_all_files(directory)
+        files = self._filter_files(files)
 
-    for dir_data in dir_walker:
-        path_to_dir = dir_data[0]
-        filenames   = dir_data[2]
+        self.database = self._organise_into_dates(files)
 
-        bgp_files += __filter_valid_files(path_to_dir, filenames, regex)
+    def _load_all_files(self, directory):
+        files  = []
 
-    return bgp_files
+        for file_data in walk(directory):
+            path      = file_data[0]
+            filenames = file_data[2]
+            dir_files = [path + "/" + filename for filename in filenames]
+            files     += dir_files
 
-def __filter_valid_files(path_to_dir, filenames, regex):
-    matching_files = []
+        return files
 
-    for filename in filenames:
-        if regex.match(filename) is not None:
-            matching_files.append(path_to_dir + "/" + filename)
+    def _filter_files(self, files):
+        return [file for file in files if f.endswith(".bz2")]
 
-    return matching_files
+    def _organise_into_dates(self, files):
+        chrono_listing = {}
+
+        for filename in files:
+            yy, mm, dd, hh = self._organise_file(filename)
+
+            if yy is None:
+                continue
+
+            if (yy, mm, dd, hh) not in chrono_listing:
+                chrono_listing[(yy, mm, dd, hh)] = set()
+
+            chrono_listing[(yy, mm, dd, hh)].add(filename)
+
+        return chrono_listing
+
+    def _organise_file(self, filename):
+        if "oix" in filename:
+            return self._map_oix_filename(filename)
+        elif "RIBS" in filename:
+            return self._map_routeviews_filename(filename)
+
+        return (None, None, None, None)
+
+    def _map_oix_filename(self, filename):
+        tokens = filename.split("-")
+
+        year  = int(tokens[-4])
+        month = int(tokens[-3])
+        date  = int(tokens[-2])
+        hour  = int(tokens[-1].split(".")[0][0:2])
+
+        return year, month, date, hour
+
+    def _map_routeviews_filename(self, filename):
+        tokens = filename.split(".")
+
+        year  = int(tokens[-3][0:4])
+        month = int(tokens[-3][4:6])
+        date  = int(tokens[-3][6:8])
+        hour  = int(tokens[-2][0:2])
+
+        return year, month, date, hour
