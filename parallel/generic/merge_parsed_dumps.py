682c1abc6fad0a7d704709e76974b7f100eddfe2
Donald AC Martin <donald@martin.scot>
2015-01-09 18:38:49 +0000

    .

--- parallel/generic/merge_parsed_dumps.py
+++ parallel/generic/merge_parsed_dumps.py
@@ -21,7 +21,7 @@ def merge_dumps(files):
 
     if len(files) < 2:
         parser = load_object("temp/parsed", files[0])
-        save_object(name, parser)
+        save_object("temp/merged", name, parser)
         return
 
     parser1 = load_object("temp/parsed", files.pop())
