a529fd8b191ab039dcc4a48c276e4e47468998c6
Donald AC Martin <donald@martin.scot>
2015-01-09 18:09:15 +0000

    .

--- scripts/yearly_address_space.sh
+++ scripts/yearly_address_space.sh
@@ -10,21 +10,22 @@ export PYTHONPATH=:$HOME/honours-project
 rm -rf temp
 
 echo "Setting up directories"
+mkdir logs
 mkdir temp
 mkdir temp/parsed
 mkdir temp/merged
 
 echo "Locating available BGP files to parse"
-python $GENERIC/list_year_end_files.py
+python $GENERIC/list_year_end_files.py > logs/listing_files.log
 
 echo "Parsing files"
-parallel --no-notice -a temp/files_to_parse "python $GENERIC/parse_bgp_file.py"
+parallel --no-notice -a temp/files_to_parse "python $GENERIC/parse_bgp_file.py" > logs/parsing_files.log
 
 echo "Merging parsed router data"
-parallel --no-notice -a temp/files_to_merge "python $GENERIC/merge_parsed_dumps.py"
+parallel --no-notice -a temp/files_to_merge "python $GENERIC/merge_parsed_dumps.py" > logs/merging_files.log
 
 echo "Generating chart"
-python $PARALLEL/draw_yearly_address_space.py
+python $PARALLEL/draw_yearly_address_space.py > logs/drawing_chart.log
 
 echo "Cleaning Up"
 unset PYTHONPATH
