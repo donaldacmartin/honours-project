342af7440a2f9c4f16aeb5667522584791fa744d
Donald AC Martin <donald@martin.scot>
2015-01-09 19:11:43 +0000

    .

--- parallel/generic/list_year_end_files.py
+++ parallel/generic/list_year_end_files.py
@@ -15,7 +15,7 @@ def create_list_of_files():
     database = FileBrowser(root_dir)
     years    = []
 
-    for year in range(1997, 2000):
+    for year in range(1997, 2015):
         years.append(database.get_year_end_files(year))
 
     write_index_for_parallel_parsing(years)
