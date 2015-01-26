f9d356acdebeb6b401a5570c2ccc7ba4991156b0
Donald AC Martin <donald@martin.scot>
2015-01-26 19:15:38 +0000

    .

--- graphs/chart/base.py
+++ graphs/chart/base.py
@@ -4,12 +4,12 @@ class BaseChart(BaseGraph):
     def __init__(self, width, height):
         super(BaseChart, self).__init__(width, height)
 
-    def draw_axis(self):
+    def draw_axes(self):
         img_width, img_height = self.image.size
 
         origin     = (img_width * 0.1, img_height * 0.9)
         x_axis_end = (img_width * 0.1, img_height * 0.1)
         y_axis_end = (img_width * 0.9, img_height * 0.9)
 
-        self.draw_line(origin, x_axis_end)
-        self.draw_line(origin, y_axis_end)
+        self.draw_line(origin, x_axis_end, width=2)
+        self.draw_line(origin, y_axis_end, width=2)
---
+++ graphs/chart/cidr_block_allocs.py
@@ -0,0 +1,30 @@
+from future import __division__
+from base import BaseChart
+
+DEFAULT = ("0.0.0.0", "255.255.255.255")
+TESTING = ("4.0.0.0", "4.255.255.255")
+
+class CIDRBlockAllocs(BaseChart):
+    def __init__(self, bgp_dumps, bounds=DEFAULT, width=1920, height=1080):
+        super(CIDRBlockAllocs, self).__init__(width, height)
+
+        self.base     = ip_to_int(bounds[0])
+        self.limit    = ip_to_int(bounds[1])
+        self.ip_range = limit / base
+        row_delta     = self.image.size[1] / len(bgp_dumps)
+        row_pos       = self.image.size[1] * 0.1
+
+        for bgp_dump in bgp_dumps:
+            blocks = bgp_dump.visible_blocks
+            blocks = [(ip, cidr) for (ip,cidr) in blocks if base <= ip <= limit]
+            self.draw_row(blocks, row_pos)
+            row_pos += row_delta
+
+    def draw_row(self, blocks, row_pos)
+        for (ip, cidr) in blocks:
+            start = self.ip_to_x_coord(ip)
+            end   = self.ip_to_x_coord((ip + cidr_to_int(cidr))
+            self.draw_line((start, row_pos), (end, row_pos))
+
+    def ip_to_x_coord(self, ip):
+        return (ip - self.base) * self.ip_range
