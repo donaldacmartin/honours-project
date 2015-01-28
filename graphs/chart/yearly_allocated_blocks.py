161ab934bcbba5d9780ce3b68522140ad76cef91
Donald AC Martin <donald@martin.scot>
2015-01-28 20:18:15 +0000

    .

--- graphs/chart/yearly_allocated_blocks.py
+++ graphs/chart/yearly_allocated_blocks.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     database = FileBrowser(root_dir)
     years    = []
 
-    for year in range(2001, 2002):
+    for year in range(1997, 2014):
         years.append(database.get_year_end_files(year))
 
     years   = [year for year in years if year is not None]
