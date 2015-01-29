5b6ca185d8bc9810923d453a24157a7de4d8cec5
Donald AC Martin <donald@martin.scot>
2015-01-29 18:50:23 +0000

    .

--- graphs/base.py
+++ graphs/base.py
@@ -35,7 +35,7 @@ class BaseGraph(object):
         temp_img = new("RGB", self.arial100.getsize(text))
         draw_txt = Draw(temp_img)
         draw_txt.text((0,0), text, font=self.arial100, fill=colour)
-        rotated_txt = draw_txt.rotate(rotation,  expand=1)
+        rotated_txt = temp_img.rotate(rotation,  expand=1)
         self.image.paste(xy, rotated_txt)
 
     def save(self, filename, filetype="PNG"):
