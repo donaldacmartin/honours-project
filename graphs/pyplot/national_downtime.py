8c7ead1277cee6efd6f669d1495e24a09298f080
Donald AC Martin <donald@martin.scot>
2015-02-02 14:30:54 +0000

    .

--- graphs/pyplot/national_downtime.py
+++ graphs/pyplot/national_downtime.py
@@ -9,8 +9,8 @@ class NationalDownTimeChart(object):
 
         dates          = [bgp_dump.datetime for bgp_dump in bgp_dumps]
         vis_blocks     = [bgp.visible_blocks for bgp in bgp_dumps]
-        country_blocks = [national(block, country_iso) for block in vis_blocks]
-        ip_space       = [total_space(block) for block in country_blocks]
+        country_blocks = [self.national(block, country_iso) for block in vis_blocks]
+        ip_space       = [self.total_space(block) for block in country_blocks]
 
         # num_cxns = [len(bgp_dump.asys_connections) for bgp_dump in bgp_dumps]
 
@@ -18,7 +18,7 @@ class NationalDownTimeChart(object):
         # self.draw("Number of Connections between Autonomous Systems", "Visible Connections", dates, num_cxns, "asys_cxns.png")
 
     def national(self, blocks, country_iso):
-        return [(ip,_) for (ip,_) in blocks if in_country(ip, country_iso)]
+        return [(ip,_) for (ip,_) in blocks if self.in_country(ip, country_iso)]
 
     def in_country(self, ip_addr, country_iso):
         return self.geo.get_country_for_ip(ip_addr) == country_iso
