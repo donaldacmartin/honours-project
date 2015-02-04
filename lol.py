7d8c65c98a4ae80eba8998c36556885805118132
Donald AC Martin <donald@martin.scot>
2015-02-04 10:11:13 +0000

    .

--- lol.py
+++ lol.py
@@ -7,15 +7,7 @@ files = ["rib.20110127.2000.bz2",
          "rib.20110128.0000.bz2",
          "rib.20110128.0200.bz2",
          "rib.20110128.0400.bz2",
-         "rib.20110128.0600.bz2",
-         "rib.20110128.0800.bz2",
-         "rib.20110128.1000.bz2",
-         "rib.20110128.1200.bz2",
-         "rib.20110128.1400.bz2",
-         "rib.20110128.1600.bz2",
-         "rib.20110128.1800.bz2",
-         "rib.20110128.2000.bz2",
-         "rib.20110128.2200.bz2",]
+         "rib.20110128.0600.bz2"]
 
 parsers = [MRTParser(base_dir + file) for file in files]
 n = NationalDownTimeChart(parsers, "EGY")
