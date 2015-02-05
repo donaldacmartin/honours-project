fe3425e3715a5e1f9b9d73cd3984941d4a67a9db
Donald AC Martin <donald@martin.scot>
2015-02-05 09:01:13 +0000

    .

--- lol.py
+++ lol.py
@@ -1,13 +1,12 @@
 from utilities.parser.mrt import MRTParser
 from graphs.pyplot.national_downtime import NationalDownTimeChart
 
-base_dir = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/2011.01/RIBS/"
-files = ["rib.20110127.2000.bz2",
-         "rib.20110127.2200.bz2",
-         "rib.20110128.0000.bz2",
-         "rib.20110128.0200.bz2",
-         "rib.20110128.0400.bz2",
-         "rib.20110128.0600.bz2"]
+base_dir = "/nas05/users/csp/routing-data/archive.routeviews.org/bgpdata/2011.02/RIBS/"
+files = ["rib.20110218.2000.bz2",
+         "rib.20110218.2200.bz2",
+         "rib.20110219.0000.bz2",
+         "rib.20110219.0200.bz2",
+         "rib.20110219.0400.bz2"]
 
 parsers = [MRTParser(base_dir + file) for file in files]
-n = NationalDownTimeChart(parsers, "EGY")
+n = NationalDownTimeChart(parsers, "LBY")
