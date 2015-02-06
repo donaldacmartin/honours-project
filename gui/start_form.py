02c07422953461724e12d60169570e17d604622b
Donald AC Martin <donald@martin.scot>
2015-02-06 22:46:25 +0000

    .

--- gui/start_form.py
+++ gui/start_form.py
@@ -6,7 +6,7 @@ entry_text = ("Welcome to the University of Glasgow's Map of the Internet "
 
 class StartForm(Form):
     def create(self):
-        self.add(MultiLineEdit, value=entry_text, max_height=2, editable=False)
+        self.txt = self.add(MultiLineEdit, value=entry_text, max_height=2, editable=False)
         self.add(TitleSelectOne,
                  name="Visualisation Type: ",
                  values=["Geographical Atlas",
@@ -14,6 +14,8 @@ class StartForm(Form):
                          "Ring Graph",
                          "Yearly Chart"],
                  scroll_exit=True)
+
+        self.txt.display()
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
