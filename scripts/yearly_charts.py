2e7b329500fa213a191bc71eb5fd44ca286c7513
Donald AC Martin <donald@martin.scot>
2015-02-06 18:08:41 +0000

    .

---
+++ scripts/yearly_charts.py
@@ -0,0 +1,35 @@
+from utilities.file.search import FileBrowser
+
+PARALLEL   = ["parallel", "--no-notice"]
+PYTHON     = ["python"]
+BGP_PARSER = ["parallel/parse_bgp.py"]
+BGP_MERGER = ["parallel/merge_bgp.py"]
+SEPARATOR  = [":::"]
+
+PARALLEL_PARSER = PARALLEL + PYTHON + BGP_PARSER + SEPARATOR
+PARALLEL_MERGER = PARALLEL + PYTHON + BGP_MERGER + SEPARATOR
+
+def get_list_of_files():
+    root_dir   = "/nas05/users/csp/routing-data/archive.routeviews.org"
+    files      = FileBrowser(root_dir)
+    year_files = [files.get_year_end_files(year) for year in range(1997, 2014)]
+    return [file for file in year_files if file is not None]
+
+def organise_to_merge(year):
+    merge_format = ""
+
+    for bgp_file in year:
+        merge_format += bgp_file + "|"
+
+    return merge_format
+
+all_files = get_list_of_files()
+
+files_to_parse = [bgp_file for year in all_files for bgp_file in year]
+files_to_merge = [organise_to_merge(year) for year in all_files]
+
+print("Parsing files")
+call(PARALLEL_PARSER + files_to_parse)
+
+print("Merging parsed files")
+call(PARALLEL_MERGER + years_to_merge)
