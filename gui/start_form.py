079a45bb16bbf8889c9499ebbab9404fb861b708
Donald AC Martin <donald@martin.scot>
2015-02-06 21:55:27 +0000

    .

--- gui/start_form.py
+++ gui/start_form.py
@@ -2,8 +2,8 @@ from npyscreen import Form, TitleText, TitleSelectOne
 
 class StartForm(Form):
     def create(self):
-        self.add(TitleText, name="Hello", value="Hello"
-        self.add(TitleSelectOne, name="GraphType", values=["Map1", "Map2"], scroll_exit=True)
+        self.add(TitleText, name="Hello", value="Hello")
+        self.add(TitleSelectOne, name="GraphType", values=["Map1", "Map2"], scroll_exit=true)
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
