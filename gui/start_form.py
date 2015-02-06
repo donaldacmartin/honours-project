d2714cb792c0ff6dc438d2175cd12ac0753dea20
Donald AC Martin <donald@martin.scot>
2015-02-06 22:22:41 +0000

    .

--- gui/start_form.py
+++ gui/start_form.py
@@ -6,7 +6,7 @@ entry_text = ("Welcome to the University of Glasgow's Map of the Internet "
 
 class StartForm(Form):
     def create(self):
-        self.add(Pager, value=entry_text, max_height=5, rely=9)
+        self.add(Pager, value=entry_text, max_height=4, editable=False)
         self.add(TitleSelectOne,
                  name="Visualisation Type: ",
                  values=["Geographical Atlas",
