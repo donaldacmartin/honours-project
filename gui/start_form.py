126ee2bd9a06f2327775a022f527c68d9f2fc5b2
Donald AC Martin <donald@martin.scot>
2015-02-06 22:11:26 +0000

    .

--- gui/start_form.py
+++ gui/start_form.py
@@ -1,12 +1,19 @@
 from npyscreen import Form, FixedText, TitleSelectOne
 
-entry_text = """Welcome to the University of Glasgow's Map of the Internet
-                application for the year 2014/15."""
+entry_text = "Welcome to the University of Glasgow's Map of the Internet " +
+             "application for the year 2014/15. To proceed, please select " +
+             "of the options below."
 
 class StartForm(Form):
     def create(self):
         self.add(FixedText, value=entry_text)
-        self.add(TitleSelectOne, name="GraphType", values=["Map1", "Map2"], scroll_exit=True)
+        self.add(TitleSelectOne,
+                 name="Visualisation Type: ",
+                 values=["Geographical Atlas",
+                         "Internet Outage Diagram",
+                         "Ring Graph",
+                         "Yearly Chart"],
+                 scroll_exit=True)
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
