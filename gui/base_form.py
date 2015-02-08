8be1b52612a638ca82f0ea87fee3a70c0f48108b
Donald AC Martin <donald@martin.scot>
2015-02-08 18:49:44 +0000

    .

--- gui/base_form.py
+++ gui/base_form.py
@@ -3,6 +3,6 @@ from textwrap import wrap
 
 class BaseForm(Form):
     def add_wrapped_text(self, text):
-        lines = wrap(text, self.columns)
+        lines = wrap(text, self.columns - 4)
         text  = "\n".join(lines)
         self.add(MultiLineEdit, value=text, max_height=len(lines), editable=False)
