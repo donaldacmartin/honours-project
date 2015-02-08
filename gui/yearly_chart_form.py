6e67236eef56b398bba09e43400beb5a9f9b62c8
Donald AC Martin <donald@martin.scot>
2015-02-08 18:29:11 +0000

    .

--- gui/yearly_chart_form.py
+++ gui/yearly_chart_form.py
@@ -1,6 +1,10 @@
 from base_form import BaseForm
 from npyscreen import TitleText, TitleMultiSelect
 
+info_text = ("This option will produce a graph with years along the X-axis and "
+             "your chosen value along the Y-axis. Please select one or more of "
+             "the options below to continue.")
+
 class YearlyChartForm(BaseForm):
     def create(self):
         self.add(TitleText, name="Start Year: ")
@@ -8,10 +12,12 @@ class YearlyChartForm(BaseForm):
         self.add(TitleText, name="End Year: ")
         self.nextrely += 1
 
-        self.add(TitleMultiSelect, name="Charts", values=["IPv4 Address Space Usage",
-                                                    "Most Common Prefix Allocation",
-                                                    "Stacked Allocation of Prefixes",
-                                                    "Prefixes as Horizontal Lines"])
+        self.add(TitleMultiSelect,
+                 name="Desired Charts: ",
+                 values=["IPv4 Address Space Usage",
+                         "Most Common Prefix Allocation",
+                         "Stacked Allocation of Prefixes",
+                         "Prefixes as Horizontal Lines"])
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
