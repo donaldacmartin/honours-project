9b8e47e0265383b0636b31dcd384b72b2d258cc7
Donald AC Martin <donaldacmartin@gmail.com>
2015-02-18 16:45:39 +0000

    .

---
+++ utilities/as_lookup.py
@@ -0,0 +1,28 @@
+class ASLookup(object):
+    def __init__(self):
+        table = self.load_file()
+
+    def get_org_for_asys(self, asys):
+        return table[asys]
+
+    def load_file(self, filename="data/as_list.txt"):
+        table = {}
+        file  = open(filename, "r")
+        line  = file.readline()[:-1]
+
+        while line != "":
+            table = self.parse_line(line, table)
+            line  = file.readline()
+
+        file.close()
+        return table
+
+    def parse_line(self, line, table):
+        line = line.split("|")
+
+        asys = line[0].strip()
+        org  = line[1].strip()
+
+        asys = int(asys)
+        table[asys] = org
+        return table
---
+++ visualisation/pyplot/map_route.py
@@ -0,0 +1,27 @@
+from sys import path
+path.append("lib")
+
+from matplotlib import use
+use("agg")
+import networkx as draw, DiGraph
+from matplotlib.pyplot import savefig
+from utilities.as_lookup import ASLookup
+
+def map_route_to_ip(ip_addr, bgp_dump):
+    graph     = nx.DiGraph()
+    as_lookup = ASLookup()
+    paths     = bgp_dump.ip_block_path[ip_addr]
+
+    for path in paths:
+        graph.add_edge("Start", as_lookup.get_org_for_asys(path[0]))
+
+        for i in range(1, len(path)):
+            previous_hop = path[i - 1]
+            this_hop     = path[i]
+
+            previous_hop = as_lookup.get_org_for_asys(previous_hop)
+            this_hop     = as_lookup.get_org_for_asys(this_hop)
+            graph.add_edge(previous_hop, this_hop)
+
+    draw(graph)
+    savefig("trial.png")
