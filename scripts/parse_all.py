2a3202b65b5353ca27194ce6f69a724ab9fb031f
Donald AC Martin <donaldacmartin@gmail.com>
2015-02-23 21:20:43 +0000

    .

--- scripts/parse_all.py
+++ scripts/parse_all.py
@@ -9,13 +9,14 @@ root_dir     = "/nas05/users/csp/routing-data/archive.routeviews.org"
 files        = FileBrowser(root_dir)
 years        = [files.get_year_end_files(year) for year in range(1997, 2014)]
 
-parser_index = NamedTemporaryFile(mode="w", delete=True)
+parser_index = NamedTemporaryFile(mode="w", delete=False)
 
 for year in years:
     for file in years:
         parser_index.write(file[0] + "\n")
 
+parser_index.close()
+
 parallel_cmd[4] = parser_index.name
 parallel_parse  = call(parallel_cmd)
 print(parallel_parse)
-parser_index.close()
