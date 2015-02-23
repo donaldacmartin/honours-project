a68308de1f2be469b0420745bf191bad70a13132
Donald AC Martin <donaldacmartin@gmail.com>
2015-02-23 20:45:10 +0000

    .

--- parser/mrt.py
+++ parser/mrt.py
@@ -20,7 +20,7 @@ class MRTParser(Parser):
             except CIDRError as e:
                 print("Non-fatal CIDR notation error encountered: " + str(e))
             except Exception as e:
-                raise ParserError(e.value))
+                raise ParserError(e.value)
 
         self.integrity_check()
 
