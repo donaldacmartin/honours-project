f5c3c4cb086ba4a1af086b2aad80e1d541a4707f
Donald AC Martin <donaldacmartin@gmail.com>
2015-03-04 20:05:48 +0000

    .

--- test/test_file_search.py
+++ test/test_file_search.py
@@ -6,23 +6,23 @@ class TestFileSearch(TestCase):
         base_dir   = "/nas05/users/csp/routing-data/archive.routeviews.org"
         self.files = FileBrowser(base_dir)
 
-    def get_files_for_valid_time(self):
+    def test_files_for_valid_time(self):
         files_found = self.files.get_files_for_time(2014, 1, 1, 00)
         self.assertTrue(len(files_found) > 0)
 
-    def get_files_for_invalid_year(self):
+    def test_files_for_invalid_year(self):
         self.assertRaises(Exception, self.files.get_files_for_time, (1996, 1, 1, 00))
 
-    def get_files_for_invalid_month(self):
+    def test_files_for_invalid_month(self):
         self.assertRaises(Exception, self.files.get_files_for_time, (2014, 13, 1, 00))
 
-    def get_files_for_invalid_day(self):
+    def test_files_for_invalid_day(self):
         self.assertRaises(Exception, self.files.get_files_for_time, (2014, 1, 32, 00))
 
-    def get_files_for_invalid_hour(self):
+    def test_files_for_invalid_hour(self):
         self.assertRaises(Exception, self.files.get_files_for_time, (2014, 1, 1, 24))
 
-    def get_year_end_files(self):
+    def test_year_end_files(self):
         files_found = self.files.get_year_end_files(2013)
         self.assertTrue(len(files_found) > 0)
 
