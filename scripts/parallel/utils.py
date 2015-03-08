173fc65f650c4658fb01a6a6f38d315f099df19b
Donald AC Martin <donaldacmartin@gmail.com>
2015-03-08 16:49:14 +0000

    .

--- scripts/parallel/utils.py
+++ scripts/parallel/utils.py
@@ -101,7 +101,7 @@ def merge_parsers(dump_locations, groups):
         merged_parsers = f.read()
 
     merged_parsers = merged_parsers.split("\n")
-    merged_parsers = [load(open(parser, "r")) for parser in merged_parsers if parser.startswith("/")]
+    merged_parsers = [load(open(parser, "rb")) for parser in merged_parsers if parser.startswith("/")]
     return merged_parsers
 
 """
