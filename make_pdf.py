3f724519d0f1c0c630bd66685aebd77df54033ed
Donald AC Martin <donaldacmartin@gmail.com>
2015-03-18 18:02:40 +0100

    .

--- make_pdf.py
+++ make_pdf.py
@@ -6,16 +6,20 @@ from subprocess import Popen
 
 graph    = ["python", "plot_graph.py"]
 chart    = ["python", "plot_chart.py"]
+dtime    = ["python", "plot_downtime.py"]
 date     = ["01/06/2014"]
 low_res  = ["2338x2338"]
 high_res = ["5000x2338"]
 wide_res = ["7016x3947"]
 
 commands = [graph + ["STANDARD_ATLAS"] + date + high_res + ["latex/images/atlas.png"],
-            graph + ["STANDARD_RING"] + date + low_res + ["latex/images/ring_1.png"],
-            graph + ["STAGGERED_RING"] + date + low_res + ["latex/images/ring_2.png"],
-            chart + ["ADDRESS_SPACE"] + date + low_res + ["latex/images/addr_space.png"],
-            chart + ["YEARLY_BLOCKS"] + date + wide_res + ["latex/images/yr_blocks.png"]
+            graph + ["STANDARD_RING"] + date + low_res + ["latex/images/ring.png"],
+            graph + ["STAGGERED_RING"] + date + low_res + ["latex/images/staggered_ring.png"],
+            chart + ["ADDRESS_SPACE"] + date + low_res,
+            chart + ["YEARLY_BLOCKS"] + date + wide_res,
+            dtime + ["EGY"] + ["27/01/2011", "29/01/2011"] + low_res + ["latex/images/egypt.png"],
+            dtime + ["LIB"] + ["18/02/2011", "22/02/2011"] + low_res + ["latex/images/libya.png"],
+            dtime + ["IND"] + ["30/01/2008", "31/01/2008"] + low_res + ["latex/images/india.png"]
             ]
 
 child_processes = [Popen(command) for command in commands]
